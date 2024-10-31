047b982ed950af0c62402a2c0931db5804698229
Emmanuel Hugonnet <ehugonne@redhat.com>
2024-10-31 10:45:45 +0100

    Cleaning pom.xml and test to be able to release
    Signed-off-by: Emmanuel Hugonnet <ehugonne@redhat.com>

--- wildfly-ai/subsystem/src/test/java/org/wildfly/extension/ai/AISubsystemTestCase.java
+++ wildfly-ai/subsystem/src/test/java/org/wildfly/extension/ai/AISubsystemTestCase.java
@@ -2,12 +2,14 @@
  * Copyright The WildFly Authors
  * SPDX-License-Identifier: Apache-2.0
  */
-
 package org.wildfly.extension.ai;
 
 import java.util.EnumSet;
+import org.jboss.as.controller.capability.RuntimeCapability;
+import org.jboss.as.network.OutboundSocketBinding;
 
 import org.jboss.as.subsystem.test.AbstractSubsystemSchemaTest;
+import org.jboss.as.subsystem.test.AdditionalInitialization;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
@@ -26,5 +28,10 @@ public class AISubsystemTestCase extends AbstractSubsystemSchemaTest<AISubsystem
 
     public AISubsystemTestCase(AISubsystemSchema schema) {
         super(AISubsystemRegistrar.NAME, new AIExtension(), schema, AISubsystemSchema.CURRENT);
+    }
+
+    @Override
+    protected AdditionalInitialization createAdditionalInitialization() {
+        return AdditionalInitialization.withCapabilities(RuntimeCapability.resolveCapabilityName(OutboundSocketBinding.SERVICE_DESCRIPTOR, "weaviate"));
     }
 }
