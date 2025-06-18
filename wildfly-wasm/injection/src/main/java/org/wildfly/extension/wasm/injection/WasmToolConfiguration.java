444509976a70db7091d9eb38b51335d2b353fca6
Emmanuel Hugonnet <ehugonne@redhat.com>
2025-06-18 14:30:40 +0200

    Adding more details on WASM support
    Signed-off-by: Emmanuel Hugonnet <ehugonne@redhat.com>

--- wildfly-wasm/injection/src/main/java/org/wildfly/extension/wasm/injection/WasmToolConfiguration.java
+++ wildfly-wasm/injection/src/main/java/org/wildfly/extension/wasm/injection/WasmToolConfiguration.java
@@ -15,14 +15,20 @@ public class WasmToolConfiguration {
 
     private final String name;
     private final Path wasmFile;
-    private final String allowedHosts;
+    private final String[] allowedHosts;
+    private final Integer minMemoryConstraints;
+    private final Integer maxMemoryConstraint;
+    private final boolean aot;
     private final Map<String, String> config;
 
-    public WasmToolConfiguration(String name, Path wasmFile, Map<String, String> config) {
+    public WasmToolConfiguration(String name, Path wasmFile, String[] allowedHosts, Integer minMemoryConstraints, Integer maxMemoryConstraint, boolean aot, Map<String, String> config) {
         this.name = name;
         this.wasmFile = wasmFile;
         this.config = config;
-        this.allowedHosts = "*";
+        this.allowedHosts = allowedHosts;
+        this.aot = aot;
+        this.minMemoryConstraints = minMemoryConstraints;
+        this.maxMemoryConstraint = maxMemoryConstraint;
     }
 
     public String name() {
@@ -31,8 +37,14 @@ public class WasmToolConfiguration {
 
     public WasmInvoker create() {
         ManifestWasm wasm = ManifestWasm.fromFilePath(wasmFile).build();
-        Manifest manifest = Manifest.ofWasms(wasm)
-                .withOptions(new Manifest.Options().withAllowedHosts(allowedHosts).withConfig(config)).build();
+        Manifest.Options options = new Manifest.Options()
+                .withAllowedHosts(allowedHosts)
+                .withAoT(aot)
+                .withConfig(config);
+        if (minMemoryConstraints != null && maxMemoryConstraint != null) {
+            options = options.withMemoryLimits(minMemoryConstraints, maxMemoryConstraint);
+        }
+        Manifest manifest = Manifest.ofWasms(wasm).withOptions(options).build();
         final Plugin plugin = Plugin.ofManifest(manifest).withLogger(ChicoryLogger.ROOT_LOGGER).build();
         return new WasmInvoker() {
             @Override
--- wildfly-wasm/subsystem/src/main/java/org/wildfly/extension/wasm/WasmProviderRegistrar.java
+++ wildfly-wasm/subsystem/src/main/java/org/wildfly/extension/wasm/WasmProviderRegistrar.java
@@ -8,12 +8,14 @@ import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.FIL
 
 import java.util.Collection;
 import java.util.List;
+import org.jboss.as.controller.operations.validation.IntRangeValidator;
 import org.jboss.as.controller.AttributeDefinition;
 import org.jboss.as.controller.PathElement;
 import org.jboss.as.controller.ResourceDefinition;
 import org.jboss.as.controller.ResourceRegistration;
 import org.jboss.as.controller.SimpleAttributeDefinition;
 import org.jboss.as.controller.SimpleAttributeDefinitionBuilder;
+import org.jboss.as.controller.StringListAttributeDefinition;
 import org.jboss.as.controller.descriptions.ParentResourceDescriptionResolver;
 import org.jboss.as.controller.operations.validation.StringLengthValidator;
 import org.jboss.as.controller.registry.ManagementResourceRegistration;
@@ -41,9 +43,29 @@ public class WasmProviderRegistrar implements ChildResourceDefinitionRegistrar {
                     .setValidator(new StringLengthValidator(1, Integer.MAX_VALUE, true, false))
                     .setCapabilityReference(PathManager.PATH_SERVICE_DESCRIPTOR.getName())
                     .build();
+    protected static final SimpleAttributeDefinition WASM_AOT
+            = new SimpleAttributeDefinitionBuilder("use-aot", ModelType.BOOLEAN, true)
+                    .setAllowExpression(true)
+                    .setDefaultValue(ModelNode.TRUE)
+                    .build();
+    protected static final SimpleAttributeDefinition WASM_MEMORY_CONSTRAINTS_MINIMAL
+            = new SimpleAttributeDefinitionBuilder("min-memory-contraint", ModelType.INT, true)
+                    .setAllowExpression(true)
+                    .setValidator(IntRangeValidator.POSITIVE)
+                    .build();
+    protected static final SimpleAttributeDefinition WASM_MEMORY_CONSTRAINTS_MAXIMAL
+            = new SimpleAttributeDefinitionBuilder("max-memory-contraint", ModelType.INT, true)
+                    .setAllowExpression(true)
+                    .setValidator(IntRangeValidator.POSITIVE)
+                    .build();
+    protected static final StringListAttributeDefinition WASM_ALLOWED_HOSTS
+            = new StringListAttributeDefinition.Builder("allowed-hosts")
+                    .setAllowExpression(true)
+                    .setRequired(false)
+                    .build();
 
-    public static final Collection<AttributeDefinition> ATTRIBUTES = List.of(
-            WASM_PATH, WASM_RELATIVE_TO);
+    public static final Collection<AttributeDefinition> ATTRIBUTES = List.of(WASM_ALLOWED_HOSTS, WASM_AOT, 
+            WASM_MEMORY_CONSTRAINTS_MINIMAL, WASM_MEMORY_CONSTRAINTS_MAXIMAL, WASM_PATH, WASM_RELATIVE_TO);
 
     static final String WASM_TOOL = "wasm-tool";
 
