1a2d930dcacdb7faff0afb527a605cff1227f132
Emmanuel Hugonnet <ehugonne@redhat.com>
2025-06-25 10:20:29 +0200

    Registering runtime details for WASM modules and AI ChatModel
    Signed-off-by: Emmanuel Hugonnet <ehugonne@redhat.com>

---
+++ wildfly-wasm/subsystem/src/main/java/org/wildfly/extension/wasm/WasmModuleRegistrar.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2025 JBoss by Red Hat.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.extension.wasm;
+
+import org.jboss.as.controller.PathElement;
+import org.jboss.as.controller.ResourceDefinition;
+import org.jboss.as.controller.ResourceRegistration;
+import org.jboss.as.controller.descriptions.ParentResourceDescriptionResolver;
+import org.jboss.as.controller.registry.ManagementResourceRegistration;
+import org.wildfly.subsystem.resource.ChildResourceDefinitionRegistrar;
+import org.wildfly.subsystem.resource.ManagementResourceRegistrar;
+import org.wildfly.subsystem.resource.ManagementResourceRegistrationContext;
+import org.wildfly.subsystem.resource.ResourceDescriptor;
+
+public class WasmModuleRegistrar implements ChildResourceDefinitionRegistrar {
+
+    public static final String NAME = "module";
+    public static final PathElement PATH = PathElement.pathElement(NAME);
+    public static final ResourceRegistration REGISTRATION = ResourceRegistration.of(PATH);
+    private final ResourceDescriptor descriptor;
+
+    public WasmModuleRegistrar(ParentResourceDescriptionResolver parentResolver) {
+        this.descriptor = ResourceDescriptor.builder(parentResolver.createChildResolver(PATH))
+                .build();
+    }
+
+    @Override
+    public ManagementResourceRegistration register(ManagementResourceRegistration parent, ManagementResourceRegistrationContext context) {
+        ResourceDefinition definition = ResourceDefinition.builder(REGISTRATION, this.descriptor.getResourceDescriptionResolver()).build();
+        ManagementResourceRegistration registration = parent.registerSubModel(definition);
+        ManagementResourceRegistrar.of(this.descriptor).register(registration);
+        return parent;
+    }
+
+}
